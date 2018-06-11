6bf09dba8cb83efb2ac3d348af59e71b5b7d2179
yuta <>
2018-06-11 21:31:09 +0900

    add date

--- photo.rb
+++ photo.rb
@@ -1,4 +1,12 @@
 {
     "1" => "https://image.rakuten.co.jp/farm-hara/cabinet/chili/imgrc0077325795.jpg",
-    "2" => ""
+    "2" => "https://images-na.ssl-images-amazon.com/images/I/81oYG8RAumL._SL1500_.jpg",
+    "3" => "http://karadastyle.com/wp-content/uploads/2016/08/karai051.jpg",
+    "4" => "http://www.chilli.jp/shopimages/pepper/0010110000012.gif",
+    "5" => "https://cookbiz.jp/soken/core/wp-content/uploads/2016/03/karai02.jpg",
+    "6" => "http://livedoor.blogimg.jp/gigoro03/imgs/3/c/3c5b8595.jpg",
+    "7" => "http://img05.shop-pro.jp/PA01311/841/product/110553151.jpg?cmsp_timestamp=20161204022458",
+    "8" => "https://ja.wikipedia.org/wiki/%E3%83%8F%E3%83%A9%E3%83%9A%E3%83%BC%E3%83%8B%E3%83%A7#/media/File:Capsicum0.jpg",
+    "9" => "https://www.rakuten.ne.jp/gold/oishiina/vegephot/YA-S-008.jpg",
+    "10" => "https://images-na.ssl-images-amazon.com/images/I/519PLqz-syL.jpg"
 }
